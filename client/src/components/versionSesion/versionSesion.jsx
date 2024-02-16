e89ab0ee959cb9014d16dd1dc535d6e1ba73f144
alopvaz014 <alopvaz014@iesalmudeyne.es>
2024-02-16 10:58:34 +0100

    nuevo diseño de sesion, acabada parte especifica para mostrar la carta

--- client/src/components/versionSesion/versionSesion.jsx
+++ client/src/components/versionSesion/versionSesion.jsx
@@ -1,68 +1,58 @@
 import './versionSesion.css';
 import reverso from "./cartas/reverso.png";
-
 function VersionSesion({ rol, nombre }) {
-
   return (
     <div className="main-juego">
-   
+
       <div className="juego-cartas">
         {/* Contenido de sesion-juego */}
-        <h1>hola</h1>
       </div>
+
       <div className="juego-configuracion">
+
         <div className="configuracion-tareas">
-
-          <div className="tareas-nombreSesion">
-            <p>NombreSesion</p>
+          <div className="tareas-nav">
+             {/* Contenido de configuracion-nav */}
+             hola
           </div>
-
-          <div className="tareas-nombreTarea">
-
-            <div className="nombreTareaFijo">
-              {/* Contenido de nombreTareaFijo */}
-              <p>Tarea: </p>
+          <div className="tareas-tareas">
+            <div className="tareas-titulo">
+              {/* Contenido de tareas-titulo */}
+              titulo
             </div>
-
-            <div className="nombreTareaMovil">
-              {/* Contenido de nombreTareaMovil */}
-              <h4>Nombre de la tarea</h4>
+            <div className="tareas-nombre">
+              {/* Contenido de tareas-nombre */}
+              nombre
             </div>
           </div>
           <div className="tareas-botones">
-            <div className="boton-editar">
+            <div className="tareas-editar">
               {/* Contenido de boton-editar */}
-              <a href="#" class="btn btn-2">EDITAR
-              <span></span>
-            <span></span></a>
-
-
+              editar
             </div>
-            <div className="boton-revelar">
+            <div className="tareas-revelar">
               {/* Contenido de boton-revelar */}
-              <a href="#" class="btn btn-2">REVELAR
-              <span></span>
-            <span></span>
-            </a>
-
+              eliminar
             </div>
           </div>
         </div>
         <div className="configuracion-estimacion">
-  <div className="nombreEstimacionFijo">
-    <p className='esti'>Estimación:</p>
-  </div>
-  <div className="estimacion-carta">
-    <img src={reverso} alt="Carta" className="carta-img" />         
-  </div>
-  <div className="estimacion-revelar">
-    <a href="#" class="btn btn-2">REVELAR
-      <span></span>
-      <span></span>
-    </a>
-  </div>
-</div>
-</div>
+          <div className="estimacion-nombre">
+            <h2 class="border">ESTIMACION</h2>
+            <h2 class="wave">ESTIMACION</h2>
+          </div>
+          <div className="estimacion-carta">
+            <img src={reverso} alt="Carta" className="carta-img" />         
+          </div>
+          <div className="estimacion-revelar">
+            <a href="#" className="btn btn-2">
+            REVELAR
+            <span></span>
+            <span></span>
+            </a>
+          </div>  
+        </div>
+      </div>
     </div>
   );
 }
