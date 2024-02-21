b7f930b43018e29afa7b1ba19c16f38d4a510626
alopvaz014 <alopvaz014@iesalmudeyne.es>
2024-02-21 23:08:56 +0100

    usuario actual carta y nombre

--- client/src/components/probandoSesion/probandoSesion.jsx
+++ client/src/components/probandoSesion/probandoSesion.jsx
@@ -2,79 +2,35 @@ import React from 'react';
 import './probandoSesion.css';
 import reverso from "./cartas/reverso.png";
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 
-function ProbandoSesion({ setSesionCreada }) {
+function ProbandoSesion({ setSesionCreada, nombre }) {
 
   const navigate = useNavigate();
 
+  //Usuaris conectados
+  const [usuarios, setUsuarios] = useState([]);
 
   const handleSalirClick = () => {
     navigate("/crearSesion");
     setSesionCreada(false);
   };
+
   return (
     <div className="bodyStyle">
 <button className="exit-button" onClick={handleSalirClick}>Salir</button>     
  <div className="container">
         <div className="left-div"> 
         <div className="div-lista">
-          <ul>
-            <li>
-              <div className="card-item">
-                <img src={reverso} alt="Imagen 1" />
-                <div className="card-name">Juan Carlos</div>
-              </div>
-            </li>
-            <li>
-              <div className="card-item">
-                <img src={reverso} alt="Imagen 1" />
-                <div className="card-name">Juan Carlos</div>
-              </div>
-            </li>
-            <li>
-              <div className="card-item">
-                <img src={reverso} alt="Imagen 1" />
-                <div className="card-name">Juan Carlos</div>
-              </div>
-            </li>
-            <li>
-              <div className="card-item">
-                <img src={reverso} alt="Imagen 1" />
-                <div className="card-name">Juan Carlos</div>
-              </div>
-            </li>
-            <li>
-              <div className="card-item">
-                <img src={reverso} alt="Imagen 1" />
-                <div className="card-name">Juan Carlos</div>
-              </div>
-            </li>
-            <li>
-              <div className="card-item">
-                <img src={reverso} alt="Imagen 1" />
-                <div className="card-name">Juan Carlos</div>
-              </div>
-            </li>
-            <li>
-              <div className="card-item">
-                <img src={reverso} alt="Imagen 1" />
-                <div className="card-name">Juan Carlos</div>
-              </div>
-            </li>
-            <li>
-              <div className="card-item">
-                <img src={reverso} alt="Imagen 1" />
-                <div className="card-name">Juan Carlos</div>
-              </div>
-            </li>
-            <li>
-              <div className="card-item">
-                <img src={reverso} alt="Imagen 1" />
-                <div className="card-name">Juan Carlos</div>
-              </div>
-            </li>
-          </ul>
+        <ul>
+              <li>
+                <div className="card-item">
+                  <img src={reverso} alt="Imagen 1" />
+                  <div className="card-name">{nombre}</div>
+                </div>
+              </li>
+            </ul>
         </div>
           <div className="otrso-div">
           </div>
